54f0d65764ca85dbb44e55bd0627ef7aa138a1fc
hey-now <rubinand22@gmail.com>
2023-02-22 18:57:56 -0800

    adding search form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,11 +12,21 @@ export class AppComponent implements OnInit {
   constructor(private weatherService: WeatherService) {
 
   }
-
+  cityName: string = 'Long Beach'; 
   weatherData?: WeatherData;
 
   ngOnInit(): void {
-    this.weatherService.getWeatherData('Long Beach').subscribe({
+    this.getWeatherData(this.cityName);
+    this.cityName = '';
+  }
+
+  onSubmit() {
+    this.getWeatherData(this.cityName);
+    this.cityName = '';
+  }
+
+  private getWeatherData(cityName: string) {
+    this.weatherService.getWeatherData(cityName).subscribe({
       next: (response) => {
         this.weatherData = response;
         console.log(response);
