90ccbc9d59a0603c5388384dc253290b23b9034a
hey-now <rubinand22@gmail.com>
2023-02-22 16:44:54 -0800

    adding environment for fetching API data and creating model

---
+++ src/app/services/weather.service.ts
@@ -0,0 +1,24 @@
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.development';
+import { WeatherData } from '../models/weather.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WeatherService {
+
+  constructor(private http: HttpClient) { }
+
+  getWeatherData(cityName: string): Observable<WeatherData> {
+   return this.http.get<WeatherData>(environment.weatherApiBaseUrl, {
+      headers: new HttpHeaders()
+        .set(environment.XRapidAPIHostHeaderName, environment.XRapidAPIHostHeaderValue)
+        .set(environment.XRapidAPIKeyHeaderName, environment.XRapidAPIKeyHeaderValue)
+      ,
+      params: new HttpParams()
+        .set('q', cityName)
+    })
+  }
+}
