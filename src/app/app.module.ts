e799b2e1c6f0998c83f99284a7f08b20a9d167a9
hey-now <rubinand22@gmail.com>
2023-03-19 12:34:02 -0700

    refactoring and removing routing for app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
@@ -22,7 +21,6 @@ import { SecondDayComponent } from './second-day/second-day.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     HttpClientModule,
     FormsModule
   ],
